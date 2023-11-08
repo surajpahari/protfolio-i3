6f26db800b586ff3d45152e3bb648ab655981aa7
surajpahari <surajpahari152@gmail.com>
2023-11-08 13:55:14 +0545

    cleaning up

--- app/workspace/workspace1/page.tsx
+++ app/workspace/workspace1/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from 'next/link'
 import Introduction from './Introduction'
 
 const page = (props: {}) => {
@@ -8,23 +7,6 @@ const page = (props: {}) => {
       <Introduction />
     </div>
   )
-  {/*< div className = "relative h-full bg-gray-800" >
-      <div className='font-bold text-green-500 flex mb-1 opacity-30 p-1'>
-        <div>
-          [zyrex@basement&#160;&#160;<span className='text-white'>Introduction</span> &#160;]$ &#160;
-        </div>
-        <div className='text-white font-normal'>
-          ./giveIntro
-        </div>
-      </div>
-
-      <div className='absolute top-0  md:p-20 sm:p-0 sm:pt-10 h-full w-full'>
-        <div className='w-full min-h-full border border-blue-500 bg-slate-800 rounded'>
-          <Introduction />
-        </div>
-      </div>
-    </div >
-    */}
 }
 
 
