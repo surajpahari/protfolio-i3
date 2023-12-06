a18800c8eb0c12c28077d80ef082c9e7fcf9b2e4
surajpahari <surajpahari152@gmail.com>
2023-12-06 22:35:28 +0545

    Revert "Revert "Revert "some changes"""
    This reverts commit b1f62e6ccb3ae52c457d11a9a6d52d51f8880b9a.

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,5 +2,5 @@ module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "underside",
-  DB: "blog_post"
+  DB: "protfolio"
 };
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/welcome-admin', (req, res) => {
   res.send("Welcome Admin")
 })
 
-router.get('/create', (req, res) => {
+router.post('/create', (req, res) => {
   res.send("you are here")
 })
 module.exports = router
