b1f62e6ccb3ae52c457d11a9a6d52d51f8880b9a
surajpahari <surajpahari152@gmail.com>
2023-12-06 22:34:40 +0545

    Revert "Revert "some changes""
    This reverts commit ee7b8b42855db962be65f2322fe0dba24d3acad5.

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -1,29 +1,32 @@
 //import the model 
 const Blog = require('../models/blog.model.js')
+exports.test = (req, res) => {
+  res.send(req.params)
+}
 exports.create = (req, res) => {
+  res.send(req.body)
   if (!req.body) {
     res.status(400).send({
-      message: "Invalid request"
-    })
-    return
+      message: "Content can not be empty!"
+    });
   }
+
+  // Create a Tutorial
   const blog = new Blog({
     title: req.body.title,
     about: req.body.about,
     content: req.body.content,
-    uplaodDate: req.body.uplaodDate
-  })
+    uploadDate: req.body.uploadDate,
+  });
 
-  blog.create(blog, (data, error) => {
-    if (err) {
+  // Save Tutorial in the database
+  Blog.create(tutorial, (err, data) => {
+    if (err)
       res.status(500).send({
         message:
-          error.message || "Error while creating jobs"
-      })
-    }
-    else {
-      res.send(data)
-    }
-  })
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    else res.send(data);
+  });
 }
 
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-
 router.get('/welcome-admin', (req, res) => {
   res.send("Welcome Admin")
 })
 
-router.post('/create', (req, res) => {
+router.get('/create', (req, res) => {
   res.send("you are here")
 })
 module.exports = router
