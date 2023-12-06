ee7b8b42855db962be65f2322fe0dba24d3acad5
surajpahari <surajpahari152@gmail.com>
2023-12-06 22:33:44 +0545

    Revert "some changes"
    This reverts commit 39715216acc3cad168c677c822e5bfc64ef91ee8.

--- backend/controllers/blog.controller.js
+++ backend/controllers/blog.controller.js
@@ -1,32 +1,29 @@
 //import the model 
 const Blog = require('../models/blog.model.js')
-exports.test = (req, res) => {
-  res.send(req.params)
-}
 exports.create = (req, res) => {
-  res.send(req.body)
   if (!req.body) {
     res.status(400).send({
-      message: "Content can not be empty!"
-    });
+      message: "Invalid request"
+    })
+    return
   }
-
-  // Create a Tutorial
   const blog = new Blog({
     title: req.body.title,
     about: req.body.about,
     content: req.body.content,
-    uploadDate: req.body.uploadDate,
-  });
+    uplaodDate: req.body.uplaodDate
+  })
 
-  // Save Tutorial in the database
-  Blog.create(tutorial, (err, data) => {
-    if (err)
+  blog.create(blog, (data, error) => {
+    if (err) {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
-      });
-    else res.send(data);
-  });
+          error.message || "Error while creating jobs"
+      })
+    }
+    else {
+      res.send(data)
+    }
+  })
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,10 @@
 const express = require('express')
-
-const blogRoutes = require("./routes/blog.routes.js");
 const app = express()
 app.get('/', (req, res) => {
   res.status(500).json({ message: "Error" })
   res.send("This is expressJs")
 })
-app.get('/test', (req, res) => {
+app.post('/create', (req, res) => {
   res.send('la thik cha')
 })
-
-require("./routes/blog.routes.js")(app);
 app.listen(4000)
